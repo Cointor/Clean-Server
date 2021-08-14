2042b60d389bf3b6b64556a1cf4872bd82fa0793
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-13 23:19:54 -0500

    Create opensource.js

---
+++ comandos/opensource.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js'); 
+const config = require ("../config.js")
+module.exports = async (client, message, args) => {
+
+message.channel.send("https://github.com/Cointor/Clean-Server")
+
+}; 
