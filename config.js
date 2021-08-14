0f115f2131cfb038d64461b93c8f4be120fa99a6
Cointor <71720065+Cointor@users.noreply.github.com>
2021-08-13 23:16:16 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+  token: (process.env.TOKEN),
+  prefix: "set", 
+  online: "<:online:875615892371816469>",
+  offline: "<:offline:875620838467051530>",
+  verde: "GREEN",
+  rojo: "RED"
+}
+
+module.exports = config;
